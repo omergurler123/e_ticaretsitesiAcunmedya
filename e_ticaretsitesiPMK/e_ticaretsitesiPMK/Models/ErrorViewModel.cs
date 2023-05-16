f807fa1a1964008961de4e650ad31e5ccd8778b2
omergurler123 <118525805+omergurler123@users.noreply.github.com>
2023-05-16 15:41:46 +0300

    Başlangıc

---
+++ e_ticaretsitesiPMK/e_ticaretsitesiPMK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_ticaretsitesiPMK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
